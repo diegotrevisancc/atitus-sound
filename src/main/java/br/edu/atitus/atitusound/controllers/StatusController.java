d89c5484727bbf6655a1c84cb54a75012a67d450
diegotrevisancc <diego.trevisan.cc@gmail.com>
2023-11-09 20:39:55 -0300

    first commit

---
+++ src/main/java/br/edu/atitus/atitusound/controllers/StatusController.java
@@ -0,0 +1,15 @@
+package br.edu.atitus.atitusound.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+    @GetMapping
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("Application Live!");
+    }
+}
